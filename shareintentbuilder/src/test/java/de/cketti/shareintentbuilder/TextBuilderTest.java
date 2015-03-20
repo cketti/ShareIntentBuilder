2b7b60c1cbe79a6beee1e756ee832f4dc7eeedd4
cketti <cketti@gmail.com>
2015-03-20 11:42:36 +0100

    Break ShareIntentBuilderTest into two classes

--- shareintentbuilder/src/test/java/de/cketti/shareintentbuilder/TextBuilderTest.java
+++ shareintentbuilder/src/test/java/de/cketti/shareintentbuilder/TextBuilderTest.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import android.content.Intent;
-import android.net.Uri;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @RunWith(RobolectricTestRunner.class)
-public class ShareIntentBuilderTest {
+public class TextBuilderTest {
     private static final String TYPE_TEXT_PLAIN = "text/plain";
 
     @Test
@@ -67,46 +66,6 @@ public class ShareIntentBuilderTest {
         ShareIntentBuilder.newInstance().text(demoTexts);
     }
 
-    @Test
-    public void testShareStream() {
-        String type = "image/png";
-        Uri uri = Uri.parse("content://dummy/42");
-        Intent intent = ShareIntentBuilder.newInstance()
-                .stream(uri, type)
-                .build();
-
-        assertThat(intent.getAction()).isEqualTo(Intent.ACTION_SEND);
-        assertThat(intent.getType()).isEqualTo(type);
-        assertThat(intent.getStringExtra(Intent.EXTRA_TEXT)).isNull();
-        assertThat(intent.getParcelableExtra(Intent.EXTRA_STREAM)).isEqualTo(uri);
-    }
-
-    @SuppressWarnings("ConstantConditions")
-    @Test(expected = IllegalArgumentException.class)
-    public void testShareStreamWithFirstArgumentNull() {
-        ShareIntentBuilder.newInstance().stream(null, "text/plain");
-    }
-
-    @SuppressWarnings("ConstantConditions")
-    @Test(expected = IllegalArgumentException.class)
-    public void testShareStreamWithSecondArgumentNull() {
-        Uri uri = Uri.parse("content://dummy/42");
-        ShareIntentBuilder.newInstance().stream(uri, null);
-    }
-
-    @Test
-    public void testShareMultipleStreams() {
-        Uri[] uris = { Uri.parse("content://dummy/42"), Uri.parse("content://dummy/23")};
-        Intent intent = ShareIntentBuilder.newInstance()
-                .stream(uris[0], "image/png")
-                .stream(uris[1], "image/jpeg")
-                .build();
-
-        assertThat(intent.getAction()).isEqualTo(Intent.ACTION_SEND_MULTIPLE);
-        assertThat(intent.getType()).isEqualTo("image/*");
-        assertThat(intent.getStringExtra(Intent.EXTRA_TEXT)).isNull();
-        assertThat(intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM)).isEqualTo(Arrays.asList(uris));
-    }
 
     @Test
     public void testShareTextWithEmail() {
