2954995e19807e6ebe3732d31b41673604f3a6bb
cketti <cketti@gmail.com>
2015-03-20 07:06:13 +0100

    Add support for ACTION_SEND_MULTIPLE

--- shareintentbuilder/src/main/java/de/cketti/shareintentbuilder/ShareIntentBuilder.java
+++ shareintentbuilder/src/main/java/de/cketti/shareintentbuilder/ShareIntentBuilder.java
@@ -1,13 +1,17 @@
 package de.cketti.shareintentbuilder;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import android.content.Intent;
 
 
 public class ShareIntentBuilder {
     private static final String TYPE_TEXT_PLAIN = "text/plain";
 
-    private String text;
+    private List<String> texts = new ArrayList<>();
 
     public static ShareIntentBuilder newInstance() {
         return new ShareIntentBuilder();
@@ -17,16 +21,41 @@ public class ShareIntentBuilder {
     }
 
     public ShareIntentBuilder text(String text) {
-        this.text = text;
+        texts.add(text);
+        return this;
+    }
+
+    public ShareIntentBuilder text(Collection<String> texts) {
+        this.texts.addAll(texts);
         return this;
     }
 
     public Intent build() {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType(TYPE_TEXT_PLAIN);
-        intent.putExtra(Intent.EXTRA_TEXT, text);
+        if (texts.isEmpty()) {
+            throw new IllegalStateException("You need to call text() at least once.");
+        }
+
+        Intent intent = new Intent();
+        if (texts.size() > 1) {
+            setMultipleText(intent);
+        } else {
+            setSingleText(intent);
+        }
+
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
 
         return intent;
+    }
+
+    private void setSingleText(Intent intent) {
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType(TYPE_TEXT_PLAIN);
+        intent.putExtra(Intent.EXTRA_TEXT, texts.get(0));
+    }
+
+    private void setMultipleText(Intent intent) {
+        intent.setAction(Intent.ACTION_SEND_MULTIPLE);
+        intent.setType(TYPE_TEXT_PLAIN);
+        intent.putStringArrayListExtra(Intent.EXTRA_TEXT, new ArrayList<>(texts));
     }
 }
--- shareintentbuilder/src/test/java/de/cketti/shareintentbuilder/ShareIntentBuilderTest.java
+++ shareintentbuilder/src/test/java/de/cketti/shareintentbuilder/ShareIntentBuilderTest.java
@@ -1,6 +1,9 @@
 package de.cketti.shareintentbuilder;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.content.Intent;
 
 import org.junit.Test;
@@ -25,5 +28,39 @@ public class ShareIntentBuilderTest {
         assertThat(intent.getType()).isEqualTo(TYPE_TEXT_PLAIN);
         assertThat(intent.getStringExtra(Intent.EXTRA_TEXT)).isEqualTo(demoText);
         assertThat(intent.getStringExtra(Intent.EXTRA_STREAM)).isNull();
+    }
+
+    @Test
+    public void testShareMultipleTexts() {
+        String[] demoTexts = { "One text", "Another one" };
+        Intent intent = ShareIntentBuilder.newInstance()
+                .text(demoTexts[0])
+                .text(demoTexts[1])
+                .build();
+
+        assertThatMultipleTextsWorkAsExpected(intent, demoTexts);
+    }
+
+    @Test
+    public void testShareMultipleTextsAsListInput() {
+        String[] demoTexts = { "one", "two", "three" };
+        Intent intent = ShareIntentBuilder.newInstance()
+                .text(Arrays.asList(demoTexts))
+                .build();
+
+        assertThatMultipleTextsWorkAsExpected(intent, demoTexts);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testShareWithoutText() {
+        ShareIntentBuilder.newInstance().build();
+    }
+
+    private void assertThatMultipleTextsWorkAsExpected(Intent intent, String[] demoText) {
+        assertThat(intent.getAction()).isEqualTo(Intent.ACTION_SEND_MULTIPLE);
+        assertThat(intent.getType()).isEqualTo(TYPE_TEXT_PLAIN);
+        assertThat(intent.getStringArrayListExtra(Intent.EXTRA_TEXT).getClass()).isEqualTo(ArrayList.class);
+        assertThat(intent.getStringExtra(Intent.EXTRA_STREAM)).isNull();
+        assertThat(intent.getStringArrayListExtra(Intent.EXTRA_TEXT)).isEqualTo(Arrays.asList(demoText));
     }
 }
