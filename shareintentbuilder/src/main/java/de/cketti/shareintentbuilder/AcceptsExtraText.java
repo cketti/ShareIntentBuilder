1e9612f2aa13133693b33acc9dd442d485d6f842
cketti <cketti@gmail.com>
2015-03-21 23:00:06 +0100

    Make interfaces package-private

--- shareintentbuilder/src/main/java/de/cketti/shareintentbuilder/AcceptsExtraText.java
+++ shareintentbuilder/src/main/java/de/cketti/shareintentbuilder/AcceptsExtraText.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 import android.support.annotation.NonNull;
 
 
-public interface AcceptsExtraText {
+interface AcceptsExtraText {
     TextBuilder text(@NonNull String text);
     TextBuilder text(@NonNull Collection<String> texts);
 }
