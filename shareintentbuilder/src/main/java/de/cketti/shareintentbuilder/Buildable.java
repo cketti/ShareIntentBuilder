81aa7b140f5272407d71efc073004b0cbeec58ab
cketti <cketti@gmail.com>
2015-03-30 03:09:47 +0200

    Make interface Buildable package private

--- shareintentbuilder/src/main/java/de/cketti/shareintentbuilder/Buildable.java
+++ shareintentbuilder/src/main/java/de/cketti/shareintentbuilder/Buildable.java
@@ -21,7 +21,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 
 
-public interface Buildable {
+interface Buildable {
     /**
      * Create a share intent from the data in this builder.
      *
